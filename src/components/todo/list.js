8dfdce7aa3c46c93f078b21126b1d718e55d4e27
nacerillo <nicholascerillo@gmail.com>
2021-06-01 15:49:43 -0700

    starter code without Hooks

---
+++ src/components/todo/list.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class ToDoList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.list.map((item) => (
+          <li className={`complete-${item.complete.toString()}`} key={item._id}>
+            <span onClick={() => this.props.handleComplete(item._id)}>
+              {item.text}
+            </span>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+export default ToDoList;
