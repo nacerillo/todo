f536d7df6b5ef5226789cf6865a01275574e7065
nacerillo <nicholascerillo@gmail.com>
2021-06-01 16:58:45 -0700

    hooks added to form.js

--- src/components/todo/form.js
+++ src/components/todo/form.js
@@ -1,6 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
-class ToDoForm extends React.Component {
+const ToDoForm = (props) => {
+  const [item, setItem] = useState({});
+
+  const handleInputChange = (e) => {
+    /*this.setState({
+      let item = { ...item, [e.target.name]: e.target.value },
+    });*/
+    setItem({ ...item, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.target.reset();
+    props.handleSubmit(item);
+    const newitem = {};
+    setItem(newitem);
+  };
+  return (
+    <>
+      <h3>Add Item</h3>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>To Do Item</span>
+          <input
+            name="text"
+            placeholder="Add To Do List Item"
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          <span>Difficulty Rating</span>
+          <input
+            defaultValue="1"
+            type="range"
+            min="1"
+            max="5"
+            name="difficulty"
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          <span>Assigned To</span>
+          <input
+            type="text"
+            name="assignee"
+            placeholder="Assigned To"
+            onChange={handleInputChange}
+          />
+        </label>
+        <button>Add Item</button>
+      </form>
+    </>
+  );
+};
+
+export default ToDoForm;
+
+//import React from "react";
+
+/*class ToDoForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = { item: {} };
@@ -59,4 +118,4 @@ class ToDoForm extends React.Component {
   }
 }
 
-export default ToDoForm;
+export default ToDoForm;*/
