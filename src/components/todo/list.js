32ccfbdbbcc31288ea71851fcac57f91dc892a16
nacerillo <nicholascerillo@gmail.com>
2021-06-01 17:10:19 -0700

    adding hooks to list

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,4 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+const ToDoList = (props) => {
+  useEffect(() => {
+    console.log("name changed to:", props);
+  }, [props]);
+
+  return (
+    <ul>
+      {props.list.map((item) => (
+        <li className={`complete-${item.complete.toString()}`} key={item._id}>
+          <span onClick={() => props.handleComplete(item._id)}>
+            {item.text}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+};
+export default ToDoList;
+
+/*import React from "react";
 
 class ToDoList extends React.Component {
   render() {
@@ -15,4 +36,4 @@ class ToDoList extends React.Component {
     );
   }
 }
-export default ToDoList;
+export default ToDoList;*/
