e6f18dd27c5c98bca59398158befd1432057b84c
nacerillo <nicholascerillo@gmail.com>
2021-06-02 13:17:02 -0700

    updating todo

--- src/components/todo/form.js
+++ src/components/todo/form.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css"
 const ToDoForm = (props) => {
   const [item, setItem] = useState({});
 
--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import ListGroup from "react-bootstrap/ListGroup";
 const ToDoList = (props) => {
   useEffect(() => {
     console.log("name changed to:", props);
@@ -7,14 +8,20 @@ const ToDoList = (props) => {
 
   return (
     <ul>
-      {props.list.map((item) => (
-        <li className={`complete-${item.complete.toString()}`} key={item._id}>
-          <span onClick={() => props.handleComplete(item._id)}>
-            {item.text}
-          </span>
-          <button>X</button>
-        </li>
-      ))}
+      <ListGroup>
+        <>
+          {props.list.map((item) => (
+            <ListGroup.Item
+              variant={item.complete ? "danger" : "success"}
+              className={`complete-${item.complete.toString()} py-3`}
+              key={item._id}
+              onClick={() => props.handleComplete(item._id)}
+            >
+              {item.complete ? `${item.text} X` : item.text}
+            </ListGroup.Item>
+          ))}
+        </>
+      </ListGroup>
     </ul>
   );
 };
